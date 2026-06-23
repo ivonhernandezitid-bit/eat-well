84f082e3f91fbce51229df79b05c047b34d66fa8
ivonhernandezitid-bit <ivon.hernandez.itid@unipolidgo.edu.mx>
2026-06-22 19:40:23 -0600

    22/06/2026
    Integración del APK con Ngrok y capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'EAT WELL',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    url: 'https://ira-upstanding-zonally.ngrok-free.dev',
+    cleartext: true
+  }
 };
 
 export default config;
